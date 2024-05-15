759d8914a01444306fce951b9d8ca0ed317c8f14
NikBel523 <belyakov.nkt@gmail.com>
2024-05-15 15:50:03 +0300

    feat: замена изначальной модели пользователя на собственну.

--- backend/foodgram_project/foodgram_project/settings.py
+++ backend/foodgram_project/foodgram_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.app.UsersConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.FoodgramUser'
