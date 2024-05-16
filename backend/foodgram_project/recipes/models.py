bd94335b08aacadc9f554635607c4ec35b85ad18
NikBel523 <belyakov.nkt@gmail.com>
2024-05-16 16:44:54 +0300

    feat: добавлена общая модель игредиентов IngredientModel.

--- backend/foodgram_project/recipes/models.py
+++ backend/foodgram_project/recipes/models.py
@@ -20,3 +20,21 @@ class TagModel(models.Model):
         verbose_name = 'Тег'
         verbose_name_plural = 'Теги'
         ordering = ('slug',)
+
+
+class IngredientModel(models.Model):
+    """Общая модель ингридиентов."""
+
+    name = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+        verbose_name='Название ингридиента',
+    )
+
+    units = models.CharField(max_length=16)  # choices=UNITS ?
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Ингридиенты'
