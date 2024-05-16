4e153e25f190b334b6c782d3176523a3eb936e03
NikBel523 <belyakov.nkt@gmail.com>
2024-05-16 13:53:58 +0300

    feat: добавлены поля модели пользователя.

--- backend/foodgram_project/users/models.py
+++ backend/foodgram_project/users/models.py
@@ -1,6 +1,39 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+MAX_LENGTH_150 = 150
+ROLE_USER = 'user'
+ROLE_ADMIN = 'admin'
+
 
 class FoodgramUser(AbstractUser):
-    pass
+    """Модель для замены стандартной пользовательской модели."""
+
+    role = models.CharField(max_length=MAX_LENGTH_150,
+                            default=ROLE_USER,
+                            choices=(
+                                (ROLE_USER, 'user'),
+                                (ROLE_ADMIN, 'admin'),
+                            ))
+
+    avatar = models.ImageField(
+        upload_to='images/avatars/',
+        null=True,
+        default=None
+    )
+
+    @property
+    def is_admin(self):
+        return self.role == ROLE_ADMIN or self.is_superuser or self.is_staff
+
+    def save(self, *args, **kwargs):
+        if self.is_superuser:
+            self.role = ROLE_ADMIN
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering = ('username',)
