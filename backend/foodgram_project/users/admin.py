2ab331f5512f0c6a70140d25029f0813c5df06e2
NikBel523 <belyakov.nkt@gmail.com>
2024-05-16 14:34:18 +0300

    feat: настроена админка для модели FoodgramUser.

--- backend/foodgram_project/users/admin.py
+++ backend/foodgram_project/users/admin.py
@@ -3,4 +3,20 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import FoodgramUser
 
+
+class FoodgramUserAdmin(admin.ModelAdmin):
+    """Администратор пользователей."""
+
+    list_display = (
+        'username',
+        'email',
+        'role',
+    )
+
+    search_fields = (
+        'username',
+        'email',
+    )
+
+
 admin.site.register(FoodgramUser, UserAdmin)
