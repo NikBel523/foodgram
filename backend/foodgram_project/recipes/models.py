aeb21512dfbe0dd19fd41737003e863fa9706a59
NikBel523 <belyakov.nkt@gmail.com>
2024-05-16 16:24:35 +0300

    feat: добавлена модель тегов TagModel.

--- backend/foodgram_project/recipes/models.py
+++ backend/foodgram_project/recipes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+NAME_MAX_LENGTH = 150
+
+
+class TagModel(models.Model):
+    """Модель для создания тегов."""
+
+    name = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+        verbose_name='Тег',
+        unique=True,
+    )
+    slug = models.SlugField(max_length=16, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ('slug',)
