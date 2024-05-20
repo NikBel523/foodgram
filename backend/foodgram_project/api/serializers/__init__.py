afb023ec8538f3d69ba0741bb8e6c81160486e99
NikBel523 <belyakov.nkt@gmail.com>
2024-05-20 11:55:12 +0300

    feat: добавлен сериализатор для получения изображений в base64.

--- backend/foodgram_project/api/serializers/__init__.py
+++ backend/foodgram_project/api/serializers/__init__.py
@@ -2,8 +2,11 @@ from .ingredients_ser import IngredientSerializer
 from .recipes_ser import (RecipeIngredientsSerializer, RecipeSerializer,
                           TagSerializer)
 from .users_ser import UserSerializer
+from .base64_field import Base64ImageField
+
 
 __all__ = [
+    Base64ImageField,
     RecipeIngredientsSerializer,
     IngredientSerializer,
     RecipeSerializer,
