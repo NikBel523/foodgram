2567bafca70077b1578bc6aa7e0207c17d7a05b4
NikBel523 <belyakov.nkt@gmail.com>
2024-05-21 23:54:01 +0300

    fix: изменён порядок url, что устранило конфликт djoser и url users/subscriptions.

--- backend/foodgram_project/api/urls.py
+++ backend/foodgram_project/api/urls.py
@@ -9,25 +9,30 @@ app_name = 'api_v1'
 
 router_v1 = routers.DefaultRouter()
 
-router_v1.register(r'tags', TagViewSet)
-router_v1.register(r'ingredients', IngredientViewSet)
-router_v1.register(r'recipes', RecipeViewSet)
+router_v1.register('tags', TagViewSet)
+router_v1.register('ingredients', IngredientViewSet)
+router_v1.register('recipes', RecipeViewSet)
+
+
+custom_users_urls = [
+    path(
+        'subscriptions/',
+        SubscriptionListView.as_view(),
+        name='subscription-list',
+    ),
+    path('<int:id>/subscribe/', SubscribeView.as_view(), name='subscribe'),
+    path('me/avatar/', AvatarChangeView.as_view(), name='avatar-patch'),
+]
 
 
 urlpatterns = [
     path('', include(router_v1.urls)),
-    path('', include('djoser.urls')),
     path(
         'recipes/<int:recipe_id>/favorite/',
         AddFavoriteView.as_view(),
         name='add_favorite',
     ),
-    path(
-        'users/subscriptions/',
-        SubscriptionListView.as_view(),
-        name='subscription-list',
-    ),
-    path('users/<int:id>/subscribe/', SubscribeView.as_view(), name='subscribe'),
-    path('users/me/avatar/', AvatarChangeView.as_view(), name='avatar-patch'),
+    path('users/', include(custom_users_urls)),
+    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
