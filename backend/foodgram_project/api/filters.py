92d5f68ed6ea18af77f9f95fa32682c8d1d2f2bf
NikBel523 <belyakov.nkt@gmail.com>
2024-05-31 10:47:01 +0300

    refactor: в фильтры добавлены проверки пользователей, реализована фильтрация по тегам через ModelMultipleChoiceFilter

--- backend/foodgram_project/api/filters.py
+++ backend/foodgram_project/api/filters.py
@@ -1,12 +1,17 @@
 from django_filters import rest_framework as filters
 
-from recipes.models import IngredientModel, RecipeModel
+from recipes.models import IngredientModel, RecipeModel, TagModel
 
 
 class RecipeFilter(filters.FilterSet):
     """Основной фильтр для сортировки по тегам, избранному, списку покупок."""
 
-    tags = filters.CharFilter(method='filter_tags', label='Теги')
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=TagModel.objects.all(),
+        label='Теги'
+    )
     is_favorited = filters.BooleanFilter(
         method='filter_is_favorited',
         label='В избраном',
@@ -20,19 +25,17 @@ class RecipeFilter(filters.FilterSet):
         model = RecipeModel
         fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
 
-    def filter_tags(self, queryset, name, value):
-        tags = self.request.GET.getlist('tags')
-        return queryset.filter(tags__slug__in=tags).distinct()
-
     def filter_is_favorited(self, queryset, name, value):
-        if value:
-            return queryset.filter(is_favorited__gt=0)
-        return queryset.filter(is_favorited=0)
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(favorite__user=user)
+        return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
-        if value:
-            return queryset.filter(is_in_shopping_cart__gt=0)
-        return queryset.filter(is_in_shopping_cart=0)
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(shoppingcart__user=user)
+        return queryset
 
 
 class IngredientFilter(filters.FilterSet):
