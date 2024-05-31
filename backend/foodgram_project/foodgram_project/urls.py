54c3f2ec471005f49823d3f50f929544d71e52d8
NikBel523 <belyakov.nkt@gmail.com>
2024-05-31 12:08:20 +0300

    fix: временно убрал url для редиректа по короткой ссылке, для правильной обработки retrive к рецептам.

--- backend/foodgram_project/foodgram_project/urls.py
+++ backend/foodgram_project/foodgram_project/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('recipes.urls', namespace='recipes')),
     path('api/', include('api.urls', namespace='api_v1')),
+    # path('', include('recipes.urls', namespace='recipes')),
 
 ]
