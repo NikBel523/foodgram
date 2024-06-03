03576a76fb402a0c1f873e013dc8042993870ed4
NikBel523 <belyakov.nkt@gmail.com>
2024-06-03 10:29:14 +0300

    refactor: добавлен require_http_methods к redirect_to_recipe

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
+from django.views.decorators.http import require_http_methods
 
 from .models import RecipeModel
 
 
+@require_http_methods(('GET',))
 def redirect_to_recipe(request, short_link):
     """Перенаправление на полный URL рецепта по короткой ссылке."""
     recipe = get_object_or_404(RecipeModel, short_link=short_link)
